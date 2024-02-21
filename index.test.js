4978ef55bd0f20e3ccf9837234835d326b6ef36c
pmolina90 <P.a.molina90@gmail.com>
2024-02-21 13:48:34 -0500

    testing a few lines of code for CRUD express

--- index.test.js
+++ index.test.js
@@ -1,34 +1,45 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  // Write your tests here
+  const request = require("supertest");
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    const request = require('supertest');
+  test("Testing musicians endpoint", async () => {
+    // sends request to /musician endpoint
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+  });
 
-    test("Testing musicians endpoint", async () => {
-        // sends request to /musician endpoint
-        const response = await request(app).get("/musicians");
-        expect(response.statusCode).toBe(200);
-    })
-    
-    test("testing json.parse" , async () => {
-        // sends request to /musician endpoint
-        const response = await request(app).get("/musicians");
-        const responseData = JSON.parse(response.text);
-       
-    })
+  test("testing json.parse", async () => {
+    // sends request to /musician endpoint
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
+  });
+});
 
+describe(" GET /musicians/:id endpoint", () => {
+  // Write your tests here
+  const request = require("supertest");
 
-
-
-    
-})
+  it("responds with json containing the musician corresponding to the id", (done) => {
+    request(app)
+      .get("/musicians/1")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        const musician = res.body;
+        // Add your assertions here to validate the response
+        done();
+      });
+  });
+});
--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 //TODO: Create a GET /musicians route to return all musicians 
 
 
@@ -12,6 +15,14 @@ app.get("/musicians", async (req,res) => {
     const musicians =  await Musician.findAll({});
     res.json(musicians);
 })
+
+//TASK: In this assignment, create the route for the endpoint GET /musicians/:id which includes a URL parameter “id”. 
+
+app.get('/musicians/:id', async (req,res) =>{
+    const parameter = req.params.id;
+    const musician = await Musician.findByPk(parameter);
+    res.json(musician);
+});
 
 
 
